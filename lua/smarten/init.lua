f8da52808319d40924e151bbf36e073a71ea7455
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2025-06-18 09:27:47 +1000

    feat: handle qfcommands and search commands

--- lua/smarten/init.lua
+++ lua/smarten/init.lua
@@ -1,11 +1,13 @@
-local M = {}
+local M = {
+  cur_list = nil
+}
 
--- enum for selected list
+-- idx for selected list
 local search = 1
 local quickfix = 2
 local loclist = 3
 
--- enum for direction
+-- idx for direction
 local next = 1
 local prev = 2
 
@@ -24,12 +26,45 @@ cmds[loclist] = {
 }
 
 local run_cmd = function(list_idx, dir_idx)
-	local cmd = "silent " .. cmds[list_idx][dir_idx]
+	-- local cmd = "silent " .. cmds[list_idx][dir_idx]
+	local cmd = cmds[list_idx][dir_idx]
 	pcall(vim.cmd, cmd)
 end
 
+-- swap list
+local set_list = function(list_idx)
+  M.cur_list = list_idx
+end
+
+-- register autommands
+local register_qf_autocmd = function()
+  vim.api.nvim_create_autocmd("QuickFixCmdPost",  {
+    callback = function(_)
+      set_list(quickfix)
+    end
+  })
+end
+
+-- listen to keys
+local handle_search_keys = function(key, typed)
+  if string.match(key, "[/?*#]") then
+    set_list(search)
+  end
+end
+
+local register_search_onkey = function()
+  vim.on_key(
+    handle_search_keys,
+    nil,
+    nil
+  )
+end
+
+-- public
 M.setup = function()
-  M.cur_list = search
+  set_list(1)
+  register_qf_autocmd()
+  register_search_onkey()
 end
 
 M.next = function()
@@ -37,7 +72,7 @@ M.next = function()
 end
 
 M.prev = function()
-  run_cmd(M.cur_list, next)
+  run_cmd(M.cur_list, prev)
 end
 
 return M
